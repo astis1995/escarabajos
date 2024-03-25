347719b6da2209eadad8972e6b276c32127127b1
Esteban Soto Monge <estebanvsotom@gmail.com>
2024-03-25 11:29:52 -0600

    delete info

--- mysite/delete_and_upload_database.sql
+++
@@ -1,7 +0,0 @@
-USE inventario;
-DELETE FROM polls_inventario WHERE CODE < 200;
-SHOW VARIABLES LIKE "secure_file_priv";
-LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\CICIMA-beetles-general-inventory.txt'
-INTO TABLE polls_inventario
-FIELDS TERMINATED BY '\t'
-IGNORE 1 LINES;
--- mysite/inventory/tests.py
+++
@@ -1,5 +0,0 @@
-from django.test import TestCase
-
-
-#SUBIR UN NUEVO ARCHIVO PARA CREAR UNA BASE DE DATOS
-# Create your tests here.
